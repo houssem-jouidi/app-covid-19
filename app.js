560cf4c7bcbc36a2968cdd598cef7de4ed5be604
Houssem Jouidi <houcem.jouidi@gmail.com>
2020-07-02 12:25:04 +0200

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const compression = require("compression");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
